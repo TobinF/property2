6ad883f842a36a7ece29b670a1b6cd90c9c5c972
Tobin <debinfeng@outlook.com>
2021-12-16 22:16:02 +0800

    压力-密度函数，分两段拟合

---
+++ Pressure-Density/get_p_D.py
@@ -0,0 +1,88 @@
+# Standard library imports
+import os
+import numpy as np
+import ctREFPROP.ctREFPROP as ct
+import json
+
+
+
+# 加载64位的refprop  dll文件
+# r = ct.REFPROPFunctionLibrary(os.environ['RPPREFIX'],'dll')  #自动加载，需要配置环境变量RPPREFIX
+r = ct.REFPROPFunctionLibrary('D:\\Others\\Tools\\Refprop\\REFPRP64.DLL', 'dll')  #需要有64位dll文件
+r.SETPATHdll(os.environ['RPPREFIX'])
+
+# 定义初试数据
+def init_data():
+    T0 = 273.15-60
+    
+    # 组分比例
+    mix_ratio = [0.2,0.8] 
+    # 节点数
+    num = 80
+    p_start = 2000 # 2MPa
+    p_end = 40000 # 40MPa
+    p_range = np.linspace(p_start,p_end,num)
+
+    # 存放各物性参数
+    wm = np.zeros(num) # 摩尔质量比
+    D = np.zeros(num) # 密度
+    return T0,p_range,mix_ratio,wm,D
+
+# 定义get_z0函数获取T0下不同压力下的物性
+
+def get_z0(T0,p,mix_ratio,wm,D):
+    ''''
+    参数：
+        T0:一大于临界温度的温度值
+        p:一数组，包含所有要获取物性的压力节点
+        mix_ratio:混合气体、液体的摩尔比
+        wm,D,Cp,eta,tcx：分别为摩尔质量比、密度、比热容、粘度、导热系数
+    '''
+    # 使用循环读取T0下对应的热物性
+    # 指定fulid,配置摩尔组分
+    
+    r.SETUPdll(len(mix_ratio), 'HYDROGEN.FLD|METHANE.FLD', 'HMX.BNC', 'DEF')
+    r.SETREFdll("DEF",1,mix_ratio,0,0,0,0)
+    # 获取临界参数
+    Tc,Pc,Dc,ierr,trim = r.CRITPdll(mix_ratio)
+    # 存放数据
+    # stor_z0(wm,D,Cp,eta,tcx,len(p))
+
+    for i in range(len(p)):
+        # 通过REFPROP获取物性
+        D[i], Dl, Dv, x, y, q, e, h, s, Cv, Cp, w, ierr, herr = r.TPFLSHdll(T0,p[i],mix_ratio)
+        wm[i] = r.WMOLdll(mix_ratio)
+        eta, tcx, ierr, herr = r.TRNPRPdll(T0,D[i],x)
+    data_storage(p,wm,D,Tc,Pc)
+    # return wm,D,Cp,eta,tcx       
+
+def data_storage(p,wm,D,Tc,Pc):
+    data = {
+        '压力(MPa)': (p/1000).tolist(),
+        '密度(kg/m3)':(D*wm).tolist(),
+        # '比热容[J/(kg·K)]':(Cp/wm).tolist(),
+        # '粘度[microPa.s (10^-6 Pa.s)]':(eta).tolist(),
+        # '导热系数[W/(m·K)]':tcx.tolist(),
+        '临界温度(K)':Tc,
+        '临界压力(MPa)':Pc/1000
+        }
+    json_data = json.dumps(data,ensure_ascii=False)
+    with open('Pressure-Density\\Data\\original_p_D_data.json','w',encoding='utf-8') as f:       
+            f.write(json_data)
+    
+
+# 用以获取在T0下的物性
+def main():
+    T0,p_range,mix_ratio,wm,D=init_data()
+    # wm,D,Cp,eta,tcx,Tc,Pc 
+    get_z0(T0,p_range,mix_ratio,wm,D) 
+    # 存储原始数据
+    # data_storage(p_range,wm,D,Cp,eta,tcx,Tc,Pc) 
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
+    # print(__name__)
+
---
+++ Pressure-Density/p_error.py
@@ -0,0 +1,94 @@
+from math import sqrt
+from matplotlib.pyplot import plot
+import numpy as np
+import matplotlib.pylab as plt
+import json
+
+#解决中文显示问题
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+def error(z0,z):
+       e = z0-z
+       e0= e/z0
+       # print(abs(e).max())
+       print(abs(e0).max())
+       return abs(e0)
+
+def plot_fitting(p,U,u1,u2,u3,mark):
+       # 初始数据
+       e1 = error(U,u1)
+       e2 = error(U,u2)
+       e3 = error(U,u3)
+       
+       
+       plt.subplot(2,2,1)
+       plt.plot(p,e1,'r')
+       plt.plot(p,e2,'g')
+       plt.plot(p,e3,'b')
+       plt.xlabel('p',position=(1,0))
+       plt.ylabel('e',rotation=0,position=(0,1))
+       plt.title('误差')
+
+       plt.subplot(2,2,2)
+       plt.plot(p,U,'r')
+       plt.plot(p,u1,'g')
+       plt.xlabel('p',position=(1,0))
+       plt.ylabel('y1',rotation=0,position=(0,1))
+       plt.title(mark+'2')
+
+       # plt.show()
+       plt.subplot(2,2,3)
+       plt.plot(p,U,'r')
+       plt.plot(p,u2,'g')
+       plt.xlabel('p',position=(1,0))
+       plt.ylabel('y2',rotation=0,position=(0,1))
+       plt.title(mark+'3')
+
+       plt.subplot(2,2,4)
+       plt.plot(p,U,'r')
+       plt.plot(p,u3,'g')
+       plt.xlabel('p',position=(1,0))
+       plt.ylabel('y3',rotation=0,position=(0,1))
+       plt.title(mark+'4')
+       
+       plt.tight_layout()
+       plt.show()
+
+def load_fitting_data(filepath):
+       with open(filepath,'r',encoding='utf-8') as f:
+              data = json.load(f)
+       U1 = np.array(data['y1'])
+       U2 = np.array(data['y2'])
+       U3 = np.array(data['y3'])
+       return U1,U2,U3
+
+# with open('data\\p_data\\supercritical.json','r',encoding='utf-8') as f:
+with open('Pressure-Density\Data\original_p_D_data.json','r',encoding='utf-8') as f:
+              data = json.load(f)
+p = np.array(data['压力(MPa)'])
+D = np.array(data['密度(kg/m3)'])
+# Cp = np.array(data['比热容[J/(kg·K)]'])
+# eta = np.array(data['粘度[microPa.s (10^-6 Pa.s)]'])
+# tcx = np.array(data['导热系数[W/(m·K)]'])
+
+
+# d = load_fitting_data('data\\p_data\\u_fitting_D.json')
+# D1,D2,D3= load_fitting_data('data\\p_data\\u_fitting_supercritical_D.json')
+# Cp1,Cp2,Cp3 = load_fitting_data('data\\p_data\\u_fitting_supercritical_Cp.json')
+# tcx1,tcx2,tcx3= load_fitting_data('data\\p_data\\u_fitting_supercritical_tcx.json')
+# eta1,eta2,eta3= load_fitting_data('data\\p_data\\u_fitting_supercritical_eta.json')
+D1,D2,D3= load_fitting_data('Pressure-Density\\Data\\u_fitting_D.json')
+# Cp1,Cp2,Cp3 = load_fitting_data('data\\p_data\\u_fitting_Cp.json')
+# tcx1,tcx2,tcx3= load_fitting_data('data\\p_data\\u_fitting_tcx.json')
+# eta1,eta2,eta3= load_fitting_data('data\\p_data\\u_fitting_eta.json')
+# etaf = load_fitting_data('data\\p_data\\u_fitting_D.json')
+# tcxf = load_fitting_data('data\\p_data\\u_fitting_D.json')
+
+# plot_error(p,D,d)
+plot_fitting(p,D,D1,D2,D3,'密度')
+# plot_fitting(p,Cp,Cp1,Cp2,Cp3,'比热容')
+# plot_fitting(p,eta,eta1,eta2,eta3,'粘度')
+# plot_fitting(p,tcx,tcx1,tcx2,tcx3,'导热系数')
+
+# plot_error(p,tcx,tcxf)
